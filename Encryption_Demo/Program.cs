46d4dc94680cdd5974fbfe25550885a9ce452528
wasimcse12 <wasimruet12@gmail.com>
2018-11-12 11:43:47 +0600

    Updated

---
+++ Encryption_Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Same DES encryption with PBEmd5";
+            Console.WriteLine("Original Text: " + text);
+            DemoEncryption crypto = new DemoEncryption();
+            text = crypto.EncryptUsernamePassword(text);
+            Console.WriteLine("Encrypted Text: " + text);
+            text = crypto.DecryptUsernamePassword(text);
+            Console.WriteLine("Decrypted Text: " + text);
+
+        }
+    }
+}
